278afe5b780644677c30361678e0a76869f53fd1
Parlin Imanuel <parlin@alor.int.nikiniki.id>
2025-12-16 02:32:30 +0700

    remove billing id

--- gcp/dev/infrastructure/networking/project.tf
+++ gcp/dev/infrastructure/networking/project.tf
@@ -1,5 +1,5 @@
-resource "google_project" "host_project" {
+resource "google_project" "non_prod_network_prj" {
   name            = "Acme Non-Production Network"
   project_id      = "acme-non-prod-network"
-  billing_account = var.gcp-billing-acc-id
+#  billing_account = var.gcp-billing-acc-id
 }
