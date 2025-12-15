d4e879c2fa2fa6885e280718ef08a4bffa08dbfa
Parlin Imanuel <parlin@alor.int.nikiniki.id>
2025-12-16 02:39:34 +0700

    remove billing id to test permission

--- gcp/dev/infrastructure/networking/project.tf
+++ gcp/dev/infrastructure/networking/project.tf
@@ -1,5 +1,5 @@
 resource "google_project" "non-prod-net-project" {
   name            = "Acme Non-Production Network"
   project_id      = "acme-non-prod-network"
-  billing_account = var.gcp-billing-acc-id
+#  billing_account = var.gcp-billing-acc-id
 }
