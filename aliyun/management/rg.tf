2ee7a3e650669d79baf45fd853dcf43bda3b5fc7
Parlin Imanuel <pimanuel@gmail.com>
2025-12-18 11:16:50 +0700

    Create Dev Resource Group

---
+++ aliyun/management/rg.tf
@@ -0,0 +1,15 @@
+resource "alicloud_resource_manager_resource_group" "aliyun_dev_infrastructure" {
+  resource_group_name = "aliyun-dev-infrastructure"
+  display_name        = "aliyun-dev-infrastructure"
+}
+
+resource "alicloud_resource_manager_resource_group" "aliyun_dev_tribe_a" {
+  resource_group_name = "aliyun-dev-tribe-a"
+  display_name        = "aliyun-dev-tribe-b"
+}
+
+resource "alicloud_resource_manager_resource_group" "aliyun_dev_tribe_b" {
+  resource_group_name = "aliyun-dev-tribe-a"
+  display_name        = "aliyun-dev-tribe-b"
+}
+
