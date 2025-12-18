58fe935f0bfa5083bcd4ce3b31f63f9a53e9fcda
Parlin Imanuel <pimanuel@gmail.com>
2025-12-18 11:31:25 +0700

    fix typo

--- aliyun/management/rg.tf
+++ aliyun/management/rg.tf
@@ -5,11 +5,11 @@ resource "alicloud_resource_manager_resource_group" "aliyun_dev_infrastructure" 
 
 resource "alicloud_resource_manager_resource_group" "aliyun_dev_tribe_a" {
   resource_group_name = "aliyun-dev-tribe-a"
-  display_name        = "aliyun-dev-tribe-b"
+  display_name        = "aliyun-dev-tribe-a"
 }
 
 resource "alicloud_resource_manager_resource_group" "aliyun_dev_tribe_b" {
-  resource_group_name = "aliyun-dev-tribe-a"
+  resource_group_name = "aliyun-dev-tribe-b"
   display_name        = "aliyun-dev-tribe-b"
 }
 
