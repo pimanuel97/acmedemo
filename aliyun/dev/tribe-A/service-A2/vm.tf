234766b845e0e0652ab1b4d665a43f110bc07ed7
Parlin Imanuel <pimanuel@gmail.com>
2025-12-18 12:44:11 +0700

    modify SG

--- aliyun/dev/tribe-A/service-A2/vm.tf
+++ aliyun/dev/tribe-A/service-A2/vm.tf
@@ -10,18 +10,6 @@ variable "image_id" {
   default = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
 }
 
-data "alicloud_vpcs" "vpc" {
-  cidr_block = "10.11.0.0/16"
-  status     = "Available"
-}
-
-# Create a new ECS instance for a VPC
-resource "alicloud_security_group" "group" {
-  security_group_name = var.name
-  description         = "acme-service-A2-access"
-  vpc_id              = alicloud_vpcs.0.vpc.id
-}
-
 data "alicloud_zones" "zone_avail" {
   available_disk_category     = "cloud_efficiency"
   available_resource_creation = "VSwitch"
@@ -35,7 +23,7 @@ data "alicloud_vswitches" "vswitch" {
 
 resource "alicloud_instance" "service_A2_vm1" {
   availability_zone = data.alicloud_zones.default.zones.0.id
-  security_groups   = alicloud_security_group.group.*.id
+  security_groups   = [ "sg-t4n08sv86fk9pia0sjys" ]
 
   # series III
   instance_type              = var.instance_type
